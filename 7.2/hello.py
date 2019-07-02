03a5a2c7bac7cc4ea579d75c6cb404852485d614
czy0538 <czy0538@gmail.com>
2019-07-02 09:48:48 +0800

    2019年7月2日 09:48:42

--- 7.2/hello.py
+++ 7.2/hello.py
@@ -1,11 +1,25 @@
 import math
 
-# 程序1 计算圆形面积
-r = float(input("请输入半径"))
-area = math.pi * pow(r, 2)
-print("面积为" + str(area))
+# # 程序1 计算圆形面积
+# r = float(input("请输入半径"))
+# area = math.pi * pow(r, 2)
+# print("面积为" + str(area))
+#
+# # 程序2 计算直角三角形面积
+# a = int(input())
+# b = int(input())
+# print(a * b / 2)
 
-# 程序2 计算直角三角形面积
-a = int(input())
-b = int(input())
-print(a * b / 2)
+# # 打印贺卡
+# name = input("请输入发送人名字")
+# to = input("请输入接收人名字")
+# print("#################")
+# print(to)
+# print("       Merry Christmas to you")
+# print("                yours" + name)
+# print("#################")
+
+# ## 求直角三角形斜边
+# a = float(input("请输入边长"))
+# b = float(input("请输入边长"))
+# print(str((a**2+b**2)**0.5))
