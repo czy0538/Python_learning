9aeff033b6445166873a2d729680bd2740a109d5
czy0538 <czy0538@gmail.com>
2019-07-02 09:23:37 +0800

    2019年7月2日 09:23:02

---
+++ 7.2/hello.py
@@ -0,0 +1,11 @@
+import math
+
+# 程序1 计算圆形面积
+r = float(input("请输入半径"))
+area = math.pi * pow(r, 2)
+print("面积为" + str(area))
+
+# 程序2 计算直角三角形面积
+a = int(input())
+b = int(input())
+print(a * b / 2)
